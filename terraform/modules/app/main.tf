8b166a0a1221004ff362f23f85e3512d982b6479
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2018-07-08 20:25:07 +0300

    fix path to module files

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -39,12 +39,12 @@ resource "null_resource" "app" {
   }
 
   provisioner "file" {
-    source      = "files/puma.service"
+    source      = "${path.module}/files/puma.service"
     destination = "/tmp/puma.service"
   }
 
   provisioner "remote-exec" {
-    script = "files/deploy.sh"
+    script = "${path.module}/files/deploy.sh"
   }
 }
 
